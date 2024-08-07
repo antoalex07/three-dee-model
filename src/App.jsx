c6dd2ab3b4cef807be125a56c45ad09f9e5821b7
antoalex07 <antoalex710@gmail.com>
2024-08-07 23:32:37 +0530

    routes to home page and test pages added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Test from './pages/Test'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/test' element={<Test/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
