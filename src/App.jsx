57d228b0b372e356b3c5d38bb5b92a92bd71d0b8
antoalex07 <antoalex710@gmail.com>
2024-08-10 09:41:19 +0530

    new paths added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Test from './pages/test/Test'
+import Home from './pages/home/Home'
+import VideoConfig from './pages/videoConfig/VideoConfig'
+import ImageConfig from './pages/imageConfig/ImageConfig'
+import RotateConfig from './pages/rotateConfig/RotateConfig'
+import AnalyzeConfig from './pages/analyzeConfig/AnalyzeConfig'
+import ColorConfig from './pages/colorConfig/ColorConfig'
 
 function App() {
 
   return (
     <div className='app'>
       <Routes>
-        <Route path='/' element={<Test/>} />
+        <Route path='/'               element={<Home/>}           />
+        <Route path='/video-config'   element={<VideoConfig/>}    />
+        <Route path='/image-config'   element={<ImageConfig/>}    />
+        <Route path='/rotate-config'  element={<RotateConfig/>}   />
+        <Route path='/analyze-config' element={<AnalyzeConfig/>}  />
+        <Route path='/color-config'   element={<ColorConfig/>}    />
       </Routes>
     </div>
   )
