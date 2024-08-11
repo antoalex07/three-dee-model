3a53069df97c6ff7baaf1b542497bdcb49d93896
antoalex07 <antoalex710@gmail.com>
2024-08-11 18:29:45 +0530

    detailed page for model hover added

--- src/pages/imageConfig/ImageConfig.jsx
+++ src/pages/imageConfig/ImageConfig.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image360Viewer from '../../components/Image360Viewer'
+import './ImageConfig.css'
 
 const ImageConfig = () => {
   return (
-    <div>ImageConfig</div>
+    <div className='imageConfig'>
+      <div className='heading'>
+        <h1>Image Sequencing With Hover Effect</h1>
+      </div>
+      <div className='model__container'>
+        <Image360Viewer/>
+      </div>
+    </div>
   )
 }
 
