27ce144d857eb3778953e8e3c19f7209280d940b
antoalex07 <antoalex710@gmail.com>
2024-08-11 18:30:11 +0530

    detailed page for model free rotation added

--- src/pages/rotateConfig/RotateConfig.jsx
+++ src/pages/rotateConfig/RotateConfig.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import RotatingModel from '../../components/RotatingModel'
 
 const RotateConfig = () => {
   return (
-    <div>RotateConfig</div>
+    <div className='rotateConfig'>
+      <div className='heading'>
+        <h1>Rotating 3D Model</h1>
+      </div>
+      <div className='model__container'>
+        <Canvas camera={{focus: 85}}>
+          <ambientLight intensity={1}/>
+          <Suspense fallback={null}>
+            <RotatingModel/>
+          </Suspense>
+          <Environment preset='studio'/>
+        </Canvas>
+      </div>
+    </div>
   )
 }
 
