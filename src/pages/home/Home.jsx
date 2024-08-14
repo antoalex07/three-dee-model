9e06bc4d68aea73b838390c7748784031a389a54
antoalex07 <antoalex710@gmail.com>
2024-08-14 23:51:58 +0530

    paths for each div restated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,11 +7,11 @@ import { Canvas } from '@react-three/fiber'
 import RotatingModel from '../../components/RotatingModel'
 import { ContactShadows, Environment, PerspectiveCamera } from '@react-three/drei'
 import Shoe from '../../components/model/Shoe'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   
 
   return (
@@ -35,8 +35,7 @@ const Home = () => {
 
       <div className='menu'>
           
-          <div className='config'> 
-          {/* onClick={() => navigate("/video-config")}> */}
+          <div className='config' onClick={() => navigate("/video-config")}>
             <div className='model__container'>
               <VideoPlayer/>
             </div>
@@ -47,13 +46,13 @@ const Home = () => {
           {/* onClick={() => navigate("/rotate-config")}> */}
             <div className='model__container'>
               <Canvas>
-                <PerspectiveCamera makeDefault position={[2.606, 2.013, -1.729]} rotation={[-2.710, 0.920, 2.790]} fov={60} />
-                <ambientLight intensity={1}/>
+                <PerspectiveCamera makeDefault position={[2.606, 2.013, -1.729]} rotation={[-2.710, 0.920, 2.790]} fov={50} />
+                <ambientLight intensity={0.5}/>
                 <Suspense fallback={null}>
                   <RotatingModel/>
                 </Suspense>
                 <Environment files='royal.hdr' />
-                <ContactShadows rotateX={Math.PI / 3} position={[0, -0.3, 0]} opacity={0.3} width={4} height={4} blur={0.3} far={4}/>
+                <ContactShadows rotateX={Math.PI / 2} position={[0, 0, 0]} opacity={0.5} width={5} height={4} blur={0.3} far={4}/>
               </Canvas>
             </div>
             <div className='heading'>Rotating 3D Model</div>
