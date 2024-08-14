fb93a6fb55d21e608a06bc21c3a2370cfbb7445a
antoalex07 <antoalex710@gmail.com>
2024-08-14 23:50:33 +0530

    paths to other webpages reestabilished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
-// import VideoConfig from './pages/videoConfig/VideoConfig'
-// import ImageConfig from './pages/imageConfig/ImageConfig'
-// import RotateConfig from './pages/rotateConfig/RotateConfig'
-// import AnalyzeConfig from './pages/analyzeConfig/AnalyzeConfig'
-// import ColorConfig from './pages/colorConfig/ColorConfig'
+import VideoConfig from './pages/videoConfig/VideoConfig'
+import ImageConfig from './pages/imageConfig/ImageConfig'
+import RotateConfig from './pages/rotateConfig/RotateConfig'
+import AnalyzeConfig from './pages/analyzeConfig/AnalyzeConfig'
+import ColorConfig from './pages/colorConfig/ColorConfig'
 
 function App() {
 
@@ -13,11 +13,11 @@ function App() {
     <div className='app'>
       <Routes>
         <Route path='/'               element={<Home/>}           />
-        {/* <Route path='/video-config'   element={<VideoConfig/>}    />
+        <Route path='/video-config'   element={<VideoConfig/>}    />
         <Route path='/image-config'   element={<ImageConfig/>}    />
         <Route path='/rotate-config'  element={<RotateConfig/>}   />
         <Route path='/analyze-config' element={<AnalyzeConfig/>}  />
-        <Route path='/color-config'   element={<ColorConfig/>}    /> */}
+        <Route path='/color-config'   element={<ColorConfig/>}    />
       </Routes>
     </div>
   )
