90a6370427e966feafbedd5d1a3062140396bc4c
antoalex07 <antoalex710@gmail.com>
2024-09-06 16:30:38 +0530

    enabled lazy loading for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home/Home'
-import VideoConfig from './pages/videoConfig/VideoConfig'
-import ImageConfig from './pages/imageConfig/ImageConfig'
-import RotateConfig from './pages/rotateConfig/RotateConfig'
-import AnalyzeConfig from './pages/analyzeConfig/AnalyzeConfig'
-import ColorConfig from './pages/colorConfig/ColorConfig'
+import React from 'react'
+
+const Home          = React.lazy(() => import('./pages/home/Home'));
+const VideoConfig   = React.lazy(() => import('./pages/videoConfig/VideoConfig'));
+const ImageConfig   = React.lazy(() => import('./pages/imageConfig/ImageConfig'));
+const RotateConfig  = React.lazy(() => import('./pages/rotateConfig/RotateConfig'));
+const AnalyzeConfig = React.lazy(() => import('./pages/analyzeConfig/AnalyzeConfig'));
+const ColorConfig   = React.lazy(() => import('./pages/colorConfig/ColorConfig'));
 
 function App() {
 
   return (
     <div className='app'>
-      <Routes>
-        <Route path='/'               element={<Home/>}           />
-        <Route path='/video-config'   element={<VideoConfig/>}    />
-        <Route path='/image-config'   element={<ImageConfig/>}    />
-        <Route path='/rotate-config'  element={<RotateConfig/>}   />
-        <Route path='/analyze-config' element={<AnalyzeConfig/>}  />
-        <Route path='/color-config'   element={<ColorConfig/>}    />
-      </Routes>
+          <Routes>
+            <Route path='/'               element={<Home/>}           />
+            <Route path='/video-config'   element={<VideoConfig/>}    />
+            <Route path='/image-config'   element={<ImageConfig/>}    />
+            <Route path='/rotate-config'  element={<RotateConfig/>}   />
+            <Route path='/analyze-config' element={<AnalyzeConfig/>}  />
+            <Route path='/color-config'   element={<ColorConfig/>}    />
+          </Routes>
     </div>
   )
 }
